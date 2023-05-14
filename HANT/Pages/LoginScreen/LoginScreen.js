f149903b9951e7f9e9f8f027c01c71893c841630
anas12312 <anashesham000@gmail.com>
2023-05-14 12:18:19 +0300

    'anas'

---
+++ HANT/Pages/LoginScreen/LoginScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const LoginScreen = () => {
+    return (
+        <View style={{padding: 50}}>
+            <Text>anas</Text>
+        </View>
+    )
+}
+
+export default LoginScreen;
